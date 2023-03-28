431caf3dffb29a95c8a66bd202eafe81364d2cd1
STCollier <1samuel3collier@gmail.com>
2023-03-28 12:04:47 -0400

    Fixed aspect ratio

--- src/main.c
+++ src/main.c
@@ -64,9 +64,9 @@ int main() {
         glm_mat4_identity(view);
         glm_mat4_identity(model);
 
-        glm_rotate(model, glm_rad(-55.0f), (vec3) {1.0f, 0.0f, 0.0f});
+        glm_rotate(model, glfwGetTime(), (vec3) {0.0f, 0.0f, 1.0f});
         glm_translate(view, (vec3) {0.0f, 0.0f, -3.0f});
-        glm_perspective(glm_rad(45.0f), window.width / window.height, 0.1f, 100.0f, projection);
+        glm_perspective(glm_rad(45.0f), (float) window.width / (float) window.height, 0.1f, 100.0f, projection); //Make sure to convert to floats for float division
 
         setShaderMat4(mainShader, "projection", projection);
         setShaderMat4(mainShader, "view", view);
