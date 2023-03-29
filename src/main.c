88185a211c0b43d958cc5f93f7450eeef72259cc
STCollier <1samuel3collier@gmail.com>
2023-03-29 09:04:02 -0400

    ok I'll stop adjusting the camera now

--- src/main.c
+++ src/main.c
@@ -37,16 +37,19 @@ int main() {
         glm_mat4_identity(view);
         glm_mat4_identity(model);
 
-        glm_translate(view, (vec3) {0.0f, 0.0f, -5.0f});
-        glm_rotate(view, glm_rad(-55.0f), (vec3) {1.0f, 0.0f, 0.0f});
+        glm_translate(view, (vec3) {0.0f, 0.0f, -4.0f});
+        glm_rotate(view, glm_rad(55.0f), (vec3) {1.0f, 0.0f, 0.0f});
+        glm_rotate(model, glm_rad(90.0f), (vec3) {1.0f, 0.0f, 0.0f});
         glm_perspective(glm_rad(window.fov), (float) window.width / (float) window.height, 0.1f, 100.0f, projection); //Make sure to convert to floats for float division
         //glm_ortho(-1.5f, 1.5f, -1.0f, 1.0f, 0.1f, 100.0f, projection);
+
+        drawFloor();
 
         setShaderMat4(mainShader, "projection", projection);
         setShaderMat4(mainShader, "view", view);
         setShaderMat4(mainShader, "model", model);
-
-        drawFloor();
+  
+        //drawSnake();
 
         glfwSwapBuffers(window.self);
         glfwPollEvents();
